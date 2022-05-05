cdb955fe008f13eeda13ccbe2e8c731ee0f5550c
Olena-Milham <nikimilh@gmail.com>
2022-05-06 02:03:30 +0300

    task-2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -2,3 +2,29 @@ import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
 console.log(galleryItems);
+const galleryEl = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    item => `<div class="gallery__item">
+  <a class="gallery__link" href="${item.original}">
+    <img
+      class="gallery__image"
+      src="${item.preview}"
+     
+      alt="${item.description}"
+    />
+  </a>
+</div>`,
+  )
+  .join('');
+
+galleryEl.insertAdjacentHTML('afterbegin', markup);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  //   captionPosition: 'bottom', (default option)
+});
+gallery.on('show.simplelightbox', function () {
+  // do something…
+});
